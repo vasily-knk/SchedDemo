79bde632f81cd6f3a76e835503fa88c144ee0d9a
Vasya <Vasya@Vasya-PC.(none)>
2012-08-01 19:58:49 +0400

    first commit

---
+++ src/SchedDemo/scheddemo.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "scheddemo.h"
+
+SchedDemo::SchedDemo(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
+{
+}
+
+SchedDemo::~SchedDemo()
+{
+
+}
---
+++ src/SchedDemo/scheddemo.h
@@ -0,0 +1,14 @@
+#ifndef SCHEDDEMO_H
+#define SCHEDDEMO_H
+
+class SchedDemo : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	SchedDemo(QWidget *parent = 0, Qt::WFlags flags = 0);
+	~SchedDemo();
+
+};
+
+#endif // SCHEDDEMO_H
